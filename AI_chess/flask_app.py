bcb031b949faba2676370b4d05bbb0db0abed389
Sujeetkushwaha14 <121584876+Sujeetkushwaha14@users.noreply.github.com>
2024-05-03 11:13:37 -0700

    Update flask_app.py

--- AI_chess/flask_app.py
+++ AI_chess/flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from chess_engine import *
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -25,4 +26,4 @@ def test_get(tester):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='8000')
+    serve(app,host='0.0.0.0',port='8000')
